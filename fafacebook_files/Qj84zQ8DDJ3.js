55cbb6de10b2c21e5fda8a32f86edd3101f137b2
Phongsak Benjawatthanasilp <the_baroque@hotmail.com>
2015-11-06 19:19:35 +0700

    222

---
+++ fafacebook_files/Qj84zQ8DDJ3.js
@@ -0,0 +1,5 @@
+/*!CK:2248077231!*//*1446763292,*/
+
+if (self.CavalryLogger) { CavalryLogger.start_js(["bnkKv"]); }
+
+__d("SearchAwarenessEventTypes",[],function a(b,c,d,e,f,g){c.__markCompiled&&c.__markCompiled();f.exports={CLICK:"click",IMPRESSION:"impression",DISMISSAL:"dismissal",WEAK_DISMISSAL:"weak_dismissal",SYSTEM_FORCED_DISMISSAL:"system_forced_dismissal"};},null,{});
---
+++ fafacebook_files/gG0GklagbYh.js
@@ -0,0 +1,5 @@
+/*!CK:3430333511!*//*1446670948,*/
+
+if (self.CavalryLogger) { CavalryLogger.start_js(["qbrdn"]); }
+
+__d("XGamesLivePlayerStatusAsyncController",["XController"],function a(b,c,d,e,f,g){c.__markCompiled&&c.__markCompiled();f.exports=c("XController").create("\/games\/async\/liveplayers\/",{user_ids:{type:"StringVector",defaultValue:[]}});},null,{});
